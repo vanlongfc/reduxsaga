f1269523b6517f4cef44c034a91f4ce60381cb87
longqv <longqv@bfd.vn>
2023-02-20 18:20:29 +0700

    init project

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { configureStore,getDefaultMiddleware } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+// configue redux-saga
+import { applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { helloSaga } from "./sagas";
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware,
+});
+sagaMiddleware.run(helloSaga);
+
+const action = (type) => store.dispatch({ type });
+export { store };
